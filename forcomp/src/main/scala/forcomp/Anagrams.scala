beddd78011a8b75d2cb7980d15b3a5c231804234
merzbird <surprise.glacier@gmail.com>
2018-09-08 23:06:49 +0300

    week 6 / forcomp / combinations

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -84,7 +84,15 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] =
+    (occurrences foldLeft List[Occurrences](Nil)) { case (acc, (ch, count)) =>
+      acc ++ (
+          for {
+            combination <- acc
+            n <- 1 to count
+          } yield combination :+ (ch, n)
+        )
+    }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    *
